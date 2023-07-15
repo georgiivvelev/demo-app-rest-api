d57e92f6bc7d8dd280106deb55b7a5c301d1539c
Georgi IV. Velev <georgi.iv.velev@gmail.com>
2023-07-15 12:23:27 +0300

    initial commit.

---
+++ src/main/java/com/georgivelev/demoapprestapi/DemoAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.georgivelev.demoapprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgivelev/demoapprestapi/DemoAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgivelev.demoapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
